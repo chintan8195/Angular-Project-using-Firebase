93a7a775b28c61c6104c02b9bb1b74554a0883a4
Chintan Parikh <chintan8195@gmail.com>
2020-04-10 17:56:28 -0700

    Learn databinding

--- Angular-with-Firebase/src/app/app.module.ts
+++ Angular-with-Firebase/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ServerComponent } from './server/server.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
